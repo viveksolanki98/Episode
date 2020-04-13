7f0a4895a0373ca9df0a9cfcd2128389ccf56c16
vivek.solanki98 <v.solanki.vs@gmail.com>
2020-04-13 11:53:22 +0100

    Initial commit

---
+++ app/src/main/java/cwm/mobileapps/episode/MainActivity.kt
@@ -0,0 +1,12 @@
+package cwm.mobileapps.episode
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
