a5e8cd5db11dc2e275479d0c824bff36a26230c9
vivek.solanki98 <v.solanki.vs@gmail.com>
2020-04-16 22:17:46 +0100

    Alpha_0.4.3

--- app/src/main/java/cwm/mobileapps/episode/DiscoverAndSearchFragment.kt
+++ app/src/main/java/cwm/mobileapps/episode/DiscoverAndSearchFragment.kt
@@ -10,26 +10,21 @@ import android.widget.TextView
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.firebase.database.DatabaseReference
-import okhttp3.OkHttpClient
-import okhttp3.Request
+import okhttp3.*
 import org.json.JSONObject
+import java.io.IOException
 import java.util.*
+
 
 class DiscoverAndSearchFragment : androidx.fragment.app.Fragment() {
     private lateinit var database: DatabaseReference
-
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         super.onCreateView(inflater, container, savedInstanceState)
         val view = inflater?.inflate(R.layout.fragment_discover_and_search, container, false)
 
-        val shows = ArrayList<String>()
-        for (i in 1..100){
-            shows.add("Show #$i")
-        }
-
         val trendingShowsRV: RecyclerView? = view?.findViewById((R.id.trendingShows_rv))
         trendingShowsRV?.layoutManager = LinearLayoutManager(context, RecyclerView.HORIZONTAL, false)
-        trendingShowsRV?.adapter = PostsAdapter(shows)
+        displayTrending(trendingShowsRV)
 
         /*
         val homeLaunchBTN: Button? = view?.findViewById(R.id.homeLaunch_btn)
@@ -41,7 +36,6 @@ class DiscoverAndSearchFragment : androidx.fragment.app.Fragment() {
 
         val discoverSearchTitleTXT: TextView? = view?.findViewById(R.id.discoverSearchTitle_txt)
         discoverSearchTitleTXT?.text = "Discover & Search."
-
 
 
         return view
@@ -70,37 +64,34 @@ class DiscoverAndSearchFragment : androidx.fragment.app.Fragment() {
 
     }
 
-    fun getSearchResults(searchTerm : String): ArrayList<String> {
+    fun displayTrending(rView : RecyclerView?) {
         //image URL: https://image.tmdb.org/t/p/w500/lbIMe94gXNGBzlFACqbrUyEXpyN.jpg
+        var searchTerm = "strange"
         var urlSTR = "https://api.themoviedb.org/3/search/tv?api_key=9b05770b260d801f3b9e84fd281f2064&language=en-US&page=1&query=$searchTerm&include_adult=false"
         val request = Request.Builder().url(urlSTR).build()
         val client = OkHttpClient()
-        val response = client.newCall(request).execute()
-        val searchResultsObject = JSONObject(response.body?.string())
 
-        val showNames = ArrayList<String>()
-        for (i in 0 until searchResultsObject.length()) {
-            val item = searchResultsObject.getJSONObject(i.toString())
-            showNames.add(item.getString("name"))
-        }
-
-        return showNames
-        /*
-        client.newCall(request).enqueue(object: Callback{
-            override fun onFailure(call: Call, e: IOException) {
+        client.newCall(request).enqueue(object: Callback {
+            override fun onFailure(call: Call, e: IOException){
                 println("FAIL API")
-                return null
             }
 
             override fun onResponse(call: Call, response: Response) {
-                val body = response?.body?.string()
+                val body = response.body!!.string()
                 val result = JSONObject(body)
                 //val showName = result.getJSONArray("results").getJSONObject(0).getString("name")
                 println("API Search Success")
-                return result
+
+                val showNames = ArrayList<String>()
+                var searchResultsObject = result.getJSONArray("results")
+                for (i in 0 until searchResultsObject.length()) {
+                    val item = searchResultsObject.getJSONObject(i)
+                    showNames.add(item.getString("name"))
+                }
+
+                activity?.runOnUiThread(Runnable { rView?.adapter = PostsAdapter(showNames) })
+
             }
         })
-
-         */
     }
 }
