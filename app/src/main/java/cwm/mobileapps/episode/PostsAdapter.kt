5c4c7d2e53ab9721ebe1dfdfb319d84b4fa91696
vivek.solanki98 <v.solanki.vs@gmail.com>
2020-04-18 16:27:13 +0100

    Alpha_0.5.0

--- app/src/main/java/cwm/mobileapps/episode/PostsAdapter.kt
+++ app/src/main/java/cwm/mobileapps/episode/PostsAdapter.kt
@@ -1,14 +1,15 @@
 package cwm.mobileapps.episode
 
 import android.app.PendingIntent.getActivity
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.core.content.ContextCompat.startActivity
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
-import com.squareup.picasso.Picasso
 import java.security.AccessController.getContext
 
 class PostsAdapter(val posts : ArrayList<String>, val showImageLocations : ArrayList<String>) : RecyclerView.Adapter<PostsAdapter.ViewHolder>() {
@@ -17,10 +18,19 @@ class PostsAdapter(val posts : ArrayList<String>, val showImageLocations : Array
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         holder.showTitleTXT.text = posts[position]
-        Glide.with(holder.itemView.context).load("https://image.tmdb.org/t/p/w500/" + showImageLocations[position]).into(holder.showPosterIM)
+        Glide.with(holder.itemView.context).load("https://image.tmdb.org/t/p/w500/" + showImageLocations[position]).into(holder.showPosterIV)
         //https://image.tmdb.org/t/p/w500/lbIMe94gXNGBzlFACqbrUyEXpyN.jpg
         //Picasso.get().load("https://image.tmdb.org/t/p/w500/" + showImageLocations[position]).into(holder.showPosterIM);
         println("My Image: " + "https://image.tmdb.org/t/p/w500/" + showImageLocations[position])
+
+        holder.showPosterIV.setOnClickListener {
+            val intentToShowPageActivity = Intent(holder.itemView.context, ShowPageActivity::class.java)
+            intentToShowPageActivity.putExtra("show_title", posts[position])
+            intentToShowPageActivity.putExtra("show_poster", showImageLocations[position])
+            holder.itemView.context.startActivity(intentToShowPageActivity)
+        }
+
+
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int):  ViewHolder{
@@ -31,6 +41,6 @@ class PostsAdapter(val posts : ArrayList<String>, val showImageLocations : Array
 
     class ViewHolder(itemView:View) : RecyclerView.ViewHolder(itemView) {
         val showTitleTXT: TextView = itemView.findViewById(R.id.showTitle_txt)
-        val showPosterIM: ImageView = itemView.findViewById(R.id.showPoster_iv)
+        val showPosterIV: ImageView = itemView.findViewById(R.id.showPoster_iv)
     }
 }
---
+++ app/src/main/java/cwm/mobileapps/episode/ShowPageActivity.kt
@@ -0,0 +1,51 @@
+package cwm.mobileapps.episode
+
+import android.graphics.Bitmap
+import android.graphics.drawable.BitmapDrawable
+import android.graphics.drawable.Drawable
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import com.bumptech.glide.request.RequestOptions.bitmapTransform
+import com.bumptech.glide.request.target.CustomTarget
+import com.bumptech.glide.request.target.SimpleTarget
+import com.bumptech.glide.request.transition.Transition
+import jp.wasabeef.glide.transformations.BlurTransformation
+import kotlinx.android.synthetic.main.activity_show_page.*
+
+
+class ShowPageActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_show_page)
+
+        val showTitle = intent.getStringExtra("show_title")
+
+        showTitle_txt.text = showTitle
+        //https://image.tmdb.org/t/p/w500//AkFlhbRl7HZVWpIqaMPVXCuOQHI.jpg
+
+        //Glide.with(holder.itemView.context).load("https://image.tmdb.org/t/p/w500/" + showImageLocations[position]).into(holder.showPosterIV)
+
+        // Set background image
+        var posterURI = "https://image.tmdb.org/t/p/w500/" + intent.getStringExtra("show_poster")
+                Glide
+            .with(applicationContext)
+            .asBitmap()
+            .load(posterURI)
+            .apply(bitmapTransform(BlurTransformation(8, 3)))
+            .into(object : CustomTarget<Bitmap?>(100, 100) {
+                override fun onResourceReady(
+                    resource: Bitmap,
+                    transition: Transition<in Bitmap?>?
+                ) {
+                    val dr: Drawable = BitmapDrawable(resource)
+                    showPage_cl.background = dr
+                }
+
+                override fun onLoadCleared(placeholder: Drawable?) {
+                }
+            })
+    }
+}
