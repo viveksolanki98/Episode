d1a04e79997e648704b390be76ecf966e3c56021
vivek.solanki98 <v.solanki.vs@gmail.com>
2020-04-21 17:04:29 +0100

    Alpha_0.8.5

--- app/src/main/java/cwm/mobileapps/episode/RecyclerAdapterWLNextEpisode.kt
+++ app/src/main/java/cwm/mobileapps/episode/RecyclerAdapterWLNextEpisode.kt
@@ -6,10 +6,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.Switch
 import android.widget.TextView
+import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.google.android.gms.auth.api.signin.GoogleSignIn
 import okhttp3.Response
 import org.json.JSONArray
 import org.json.JSONObject
@@ -28,7 +31,10 @@ class RecyclerAdapterWLNextEpisode(val episodeIDs : ArrayList<String>) : Recycle
 
 
         APIhandler.trackitAPIAsync("https://api.trakt.tv/search/imdb/${episodeIDs[position]}?extended=full", fun (data : Response){
+            val userAccountDetails= GoogleSignIn.getLastSignedInAccount(holder.itemView.context)
+
             var dataObj = JSONArray(data.body!!.string()).getJSONObject(0)
+            val showID = dataObj.getJSONObject("show").getJSONObject("ids").getString("imdb")
             val showTitle = dataObj.getJSONObject("show").getString("title")
             val episodeTitle = dataObj.getJSONObject("episode").getString("title")
             val season = dataObj.getJSONObject("episode").getInt("season")
@@ -59,11 +65,23 @@ class RecyclerAdapterWLNextEpisode(val episodeIDs : ArrayList<String>) : Recycle
                 holder.showPosterIV.setOnClickListener {
                     val intentToShowPageActivity = Intent(holder.itemView.context, ShowPageActivity::class.java)
                     intentToShowPageActivity.putExtra("show_title", showTitle)
-                    intentToShowPageActivity.putExtra("show_id", dataObj.getJSONObject("show").getJSONObject("ids").getString("imdb"))
+                    intentToShowPageActivity.putExtra("show_id", showID)
                     intentToShowPageActivity.putExtra("show_poster", imageLocation)
 
                     holder.itemView.context.startActivity(intentToShowPageActivity)
                 }
+
+                holder.watchedToggleSWT.setOnCheckedChangeListener { _, isChecked ->
+                    if (isChecked) {
+                        FBDBhandler.addRecord(episodeIDs[position], showID, (userAccountDetails?.id)!!.toString())
+                        Toast.makeText(holder.itemView.context,"Show Marked Watched!", Toast.LENGTH_SHORT).show()
+                    }else{
+                        FBDBhandler.deleteRecord("UserID_EpisodeID", "${(userAccountDetails?.id)!!.toString()}_${episodeIDs[position]}", fun(){
+                            Toast.makeText(holder.itemView.context,"Show Marked Not Watched", Toast.LENGTH_SHORT).show()
+                        }, fun(){})
+                    }
+                }
+
             })
         })
     }
@@ -79,6 +97,7 @@ class RecyclerAdapterWLNextEpisode(val episodeIDs : ArrayList<String>) : Recycle
         val episodeTitleTXT : TextView = itemView.findViewById(R.id.episodeTitle_txt)
         val episodeDetailsTXT : TextView = itemView.findViewById(R.id.episodeDetails_txt)
         val showPosterIV : ImageView = itemView.findViewById(R.id.showPoster_iv)
+        val watchedToggleSWT : Switch = itemView.findViewById(R.id.watchedToggle_swt)
     }
 
 
