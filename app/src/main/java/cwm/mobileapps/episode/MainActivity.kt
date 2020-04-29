fed9a68061c5c5dc6713e37bc2aff03dca960815
vivek.solanki98 <v.solanki.vs@gmail.com>
2020-04-29 16:44:04 +0100

    Alpha_5.2.1

--- app/src/main/java/cwm/mobileapps/episode/MainActivity.kt
+++ app/src/main/java/cwm/mobileapps/episode/MainActivity.kt
@@ -29,13 +29,6 @@ class MainActivity : AppCompatActivity() {
     lateinit var mGoogleSignInClient: GoogleSignInClient
     private val RC_SIGN_IN = 9001
 
-    lateinit var notificationManager : NotificationManager
-    lateinit var notificationChannel : NotificationChannel
-    lateinit var builder : Notification.Builder
-    private var channelId : String = ""
-    private val description = "Test notification"
-
-
     lateinit var alarmManager: AlarmManager
 
     @RequiresApi(Build.VERSION_CODES.N)
@@ -54,67 +47,31 @@ class MainActivity : AppCompatActivity() {
             signIn()
         }
 
-        //NOTIFICATION TEST-----------------------
-        channelId = getString(R.string.package_name)
-        //createNotificationChannel()
-        //buildAndShowNotification()
-        //-----------------------------------------
-
-        //START SERVICE TEST-----------------------
-        //val intent = Intent(this,MyService::class.java)
-        //startService(intent)
-        //-----------------------------------------
-
         //ALARM MANAGER TEST-----------------------
         //https://github.com/kmvignesh/AlarmManagerExample/blob/master/app/src/main/java/com/example/vicky/alarmmanagerexample/MainActivity.kt
         alarmManager = getSystemService(Context.ALARM_SERVICE) as AlarmManager
 
-        val second = 15 * 1000
         val intentAlarm = Intent(this, MyReceiver::class.java)
         val pendingIntent = PendingIntent.getBroadcast(this, 0, intentAlarm, PendingIntent.FLAG_UPDATE_CURRENT)
-        println("appdebug: mainActivity: Create Alarm: " + Date().toString())
-        alarmManager.setExact(AlarmManager.RTC_WAKEUP, System.currentTimeMillis() + second, pendingIntent)
+        println("appdebug: mainActivity: Create Alarm: ${Date()} TRIGGER")
+
+        //From android documentation
+        // Set the alarm to start at approximately 2:00 p.m.
+
+        val calendar: Calendar = Calendar.getInstance().apply {
+            timeInMillis = System.currentTimeMillis()
+            set(Calendar.HOUR_OF_DAY, 19)
+        }
+
+        // With setInexactRepeating(), you have to use one of the AlarmManager interval
+        // constants--in this case, AlarmManager.INTERVAL_DAY.
+        alarmManager.setInexactRepeating(
+            AlarmManager.RTC_WAKEUP,
+            calendar.timeInMillis,
+            AlarmManager.INTERVAL_DAY,
+            pendingIntent
+        )
         //-----------------------------------------
-    }
-
-    private fun createNotificationChannel() {
-        // Create the NotificationChannel, but only on API 26+ because
-        // the NotificationChannel class is new and not in the support library
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            val descriptionText = "An app to track watched T.V. shows and discover new ones. Episode"
-            val channel = NotificationChannel(channelId, description, NotificationManager.IMPORTANCE_DEFAULT).apply {
-                description = descriptionText
-                enableLights(true)
-                lightColor = Color.GREEN
-                enableVibration(true)
-            }
-
-            // Register the channel with the system
-            notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-            notificationManager.createNotificationChannel(channel)
-        }
-    }
-
-    private fun buildAndShowNotification(){
-        // Create an explicit intent for an Activity in your app
-        val intent = Intent(this, UserHomeActivity::class.java).apply {
-            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
-        }
-        val pendingIntent: PendingIntent = PendingIntent.getActivity(this, 0, intent, 0)
-
-        val builder = NotificationCompat.Builder(this, channelId)
-            .setSmallIcon(R.drawable.ic_launcher_foreground)
-            .setContentTitle("My notification")
-            .setContentText("Hello World!")
-            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
-            // Set the intent that will fire when the user taps the notification
-            .setContentIntent(pendingIntent)
-            .setAutoCancel(true)
-
-        with(NotificationManagerCompat.from(this)) {
-            // notificationId is a unique int for each notification that you must define
-            notify(1234, builder.build())
-        }
     }
 
     override fun onStart() {
--- app/src/main/java/cwm/mobileapps/episode/MyService.kt
+++ app/src/main/java/cwm/mobileapps/episode/MyService.kt
@@ -1,12 +1,22 @@
 package cwm.mobileapps.episode
 
-import android.app.Service
+import android.app.*
+import android.content.Context
 import android.content.Intent
+import android.graphics.Color
+import android.os.Build
 import android.os.IBinder
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+
 //template from https://github.com/kmvignesh/MyServiceExample/blob/master/app/src/main/java/com/example/vicky/myserviceexample/MyService.kt
 class MyService : Service() {
-
     val TAG = "MyService"
+    lateinit var notificationManager : NotificationManager
+    lateinit var notificationChannel : NotificationChannel
+    lateinit var builder : Notification.Builder
+    private var channelId : String = ""
+    private val description = "Test notification"
 
     override fun onBind(intent: Intent): IBinder? {
         return null
@@ -19,11 +29,17 @@ class MyService : Service() {
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         ShowLog("onStartCommand")
+        channelId = getString(R.string.package_name)
+        createNotificationChannel()
+
 
         val runable = Runnable {
-            for (i in 1..10) {
+            for (i in 1..5) {
                 ShowLog("Service doing something." + i.toString())
                 Thread.sleep(1000)
+                if (i==5){
+                    buildAndShowNotification()
+                }
             }
             stopSelf()
         }
@@ -42,5 +58,45 @@ class MyService : Service() {
 
     fun ShowLog(message: String) {
         println("appdebug: myService: $message")
+    }
+
+    private fun createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val descriptionText = "An app to track watched T.V. shows and discover new ones. Episode"
+            val channel = NotificationChannel(channelId, description, NotificationManager.IMPORTANCE_DEFAULT).apply {
+                description = descriptionText
+                enableLights(true)
+                lightColor = Color.GREEN
+                enableVibration(true)
+            }
+
+            // Register the channel with the system
+            notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
+    }
+
+    private fun buildAndShowNotification(){
+        // Create an explicit intent for an Activity in your app
+        val intent = Intent(this, UserHomeActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        }
+        val pendingIntent: PendingIntent = PendingIntent.getActivity(this, 0, intent, 0)
+
+        val builder = NotificationCompat.Builder(this, channelId)
+            .setSmallIcon(R.drawable.ic_launcher_foreground)
+            .setContentTitle("My notification")
+            .setContentText("Hello World!")
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            // Set the intent that will fire when the user taps the notification
+            .setContentIntent(pendingIntent)
+            .setAutoCancel(true)
+
+        with(NotificationManagerCompat.from(this)) {
+            // notificationId is a unique int for each notification that you must define
+            notify(1234, builder.build())
+        }
     }
 }
