38c7f382a6101238380750d766ad190a99ad4969
vivek.solanki98 <v.solanki.vs@gmail.com>
2020-04-29 13:05:57 +0100

    Alpha_5.0.0

--- app/src/main/java/cwm/mobileapps/episode/MainActivity.kt
+++ app/src/main/java/cwm/mobileapps/episode/MainActivity.kt
@@ -2,13 +2,20 @@ package cwm.mobileapps.episode
 
 
 import android.R.attr.key
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
+import android.graphics.Color
 import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
@@ -23,6 +30,12 @@ class MainActivity : AppCompatActivity() {
     //Google sign in code from: https://johncodeos.com/how-to-add-google-login-button-to-your-android-app-using-kotlin/
     lateinit var mGoogleSignInClient: GoogleSignInClient
     private val RC_SIGN_IN = 9001
+
+    lateinit var notificationManager : NotificationManager
+    lateinit var notificationChannel : NotificationChannel
+    lateinit var builder : Notification.Builder
+    private var channelId : String = ""
+    private val description = "Test notification"
 
     @RequiresApi(Build.VERSION_CODES.N)
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -40,7 +53,52 @@ class MainActivity : AppCompatActivity() {
             signIn()
         }
 
+        //NOTIFICATION TEST-----------------------
+        channelId = getString(R.string.package_name)
+        //createNotificationChannel()
+        //buildAndShowNotification()
+        //-----------------------------------------
 
+    }
+
+    private fun createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val descriptionText = "An app to track watched T.V. shows and discover new ones. Episode"
+            val channel = NotificationChannel(channelId, description, NotificationManager.IMPORTANCE_DEFAULT).apply {
+                description = descriptionText
+                enableLights(true)
+                lightColor = Color.GREEN
+                enableVibration(true)
+            }
+
+            // Register the channel with the system
+            notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
+    }
+
+    private fun buildAndShowNotification(){
+        // Create an explicit intent for an Activity in your app
+        val intent = Intent(this, UserHomeActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        }
+        val pendingIntent: PendingIntent = PendingIntent.getActivity(this, 0, intent, 0)
+
+        val builder = NotificationCompat.Builder(this, channelId)
+            .setSmallIcon(R.drawable.ic_launcher_foreground)
+            .setContentTitle("My notification")
+            .setContentText("Hello World!")
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            // Set the intent that will fire when the user taps the notification
+            .setContentIntent(pendingIntent)
+            .setAutoCancel(true)
+
+        with(NotificationManagerCompat.from(this)) {
+            // notificationId is a unique int for each notification that you must define
+            notify(1234, builder.build())
+        }
     }
 
     override fun onStart() {
